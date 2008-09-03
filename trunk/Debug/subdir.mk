049ad90171cb5b805e147c24a7d0482b9afc9a84
Ray Speth <speth@mit.edu>
2008-09-03 22:03:32 +0000

    Added makefiles to enable building on systems without Eclipse.

---
+++ trunk/Debug/subdir.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../chemistry.cpp \
+../debugUtils.cpp \
+../flameSolver.cpp \
+../flameSys.cpp \
+../grid.cpp \
+../mathUtils.cpp \
+../matlabFile.cpp \
+../readConfig.cpp \
+../strainedFlame.cpp \
+../sundialsUtils.cpp 
+
+OBJS += \
+./chemistry.o \
+./debugUtils.o \
+./flameSolver.o \
+./flameSys.o \
+./grid.o \
+./mathUtils.o \
+./matlabFile.o \
+./readConfig.o \
+./strainedFlame.o \
+./sundialsUtils.o 
+
+CPP_DEPS += \
+./chemistry.d \
+./debugUtils.d \
+./flameSolver.d \
+./flameSys.d \
+./grid.d \
+./mathUtils.d \
+./matlabFile.d \
+./readConfig.d \
+./strainedFlame.d \
+./sundialsUtils.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	icc -I/usr/local/cantera/include -I/usr/local/matlab/extern/include -O0 -g3 -c -fmessage-length=0 -D"__sync_fetch_and_add(ptr,addend)=_InterlockedExchangeAdd(const_cast<void*>(reinterpret_cast<volatile void*>(ptr)), addend)" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ trunk/Release/objects.mk
@@ -0,0 +1,7 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lsundials_nvecserial -lhdf5 -licui18n -licuio -licuuc -licudata -lut -lboost_filesystem -lconfig++ -lmat -lmx -lsundials_ida -luser -loneD -lzeroD -lequil -lkinetics -ltransport -lthermo -lctnumerics -lsundials_cvodes -lsundials_nvecserial -lclib -lctbase -lctlapack -lctblas -lctmath -lconverters -ltpx -lctf2c -lctcxx
