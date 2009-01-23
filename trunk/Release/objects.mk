a20a4b19bf07f0c3b6c3399f129e364ace9c1427
Ray Speth <speth@mit.edu>
2009-01-23 22:08:35 +0000

    Fixes to run on the new install of Pharos

--- trunk/Release/objects.mk
+++ trunk/Release/objects.mk
@@ -4,4 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lsundials_nvecserial -lhdf5 -licui18n -licuio -licuuc -licudata -lut -lboost_filesystem -lconfig++ -lmat -lmx -lsundials_ida -luser -loneD -lzeroD -lequil -lkinetics -ltransport -lthermo -lctnumerics -lsundials_cvodes -lsundials_nvecserial -lclib -lctbase -lctmath -lconverters -ltpx -lctf2c -lctcxx -llapack -lblas
+LIBS := -lsundials_nvecserial -lhdf5 -licui18n -licuio -licuuc -licudata -lut -lboost_filesystem -lconfig++ -lmat -lmx -lsundials_ida -luser -loneD -lzeroD -lequil -lkinetics -ltransport -lthermo -lctnumerics -lsundials_cvodes -lsundials_nvecserial -lclib -lctbase -lctmath -lconverters -ltpx -lctf2c -lctcxx  -llapack -lcblas -lf77blas -latlas
+#LIBS := -lsundials_nvecserial -lhdf5 -licui18n -licuio -licuuc -licudata -lut -lboost_filesystem -lconfig++ -lmat -lmx -lsundials_ida -luser -loneD -lzeroD -lequil -lkinetics -ltransport -lthermo -lctnumerics -lsundials_cvodes -lsundials_nvecserial -lclib -lctbase -lctmath -lconverters -ltpx -lctf2c -lctcxx -llapack -lblas
