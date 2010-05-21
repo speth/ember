4086735eb1c235930a8eff7915f19439430d26e1
Ray Speth <speth@mit.edu>
2010-05-21 15:40:05 -0400

    Replaced the Managed Eclipse project with a "Makefile" project that actually runs bjam.

--- Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lsundials_nvecserial -lhdf5 -licui18n -licuio -licuuc -licudata -lut -lboost_filesystem -lconfig++ -lmat -lmx -lsundials_ida -luser -loneD -lzeroD -lequil -lkinetics -ltransport -lthermo -lctnumerics -lsundials_cvodes -lsundials_nvecserial -lclib -lctbase -lctmath -lconverters -ltpx -lctf2c -lctcxx -llapack -lblas
--- Release/updateVersionString.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export REVISION=`svn info ../flameSys.cpp | grep Revision | perl -p -e 's/^.*?(\d+)$/$1/'`
-perl -pi -e "s/std::string REVISION = \".*\";/std::string REVISION = \"$REVISION\";/" ../strainedFlame.cpp
-export DATE=`date +%F\ %T`
-perl -pi -e "s/std::string BUILDDATE = \".*\";/std::string BUILDDATE = \"$DATE\";/" ../strainedFlame.cpp
