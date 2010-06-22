f6e84640199e8e1a61571ace297468cac00c9623
Ray Speth <speth@mit.edu>
2010-06-22 17:13:17 -0400

    Added a version of CanteraGas that represents a gas mixture at a single point in space.

---
+++ src/chemistry0d.cpp
@@ -0,0 +1,178 @@
+#include "chemistry0D.h"
+#include "boost/filesystem.hpp"
+#include "debugUtils.h"
+
+CanteraGas::CanteraGas()
+    : rootXmlNode(NULL)
+    , phaseXmlNode(NULL)
+    , kinetics(NULL)
+    , multiTransport(NULL)
+    , mixTransport(NULL)
+{
+}
+
+CanteraGas::~CanteraGas()
+{
+    Cantera::close_XML_File(mechanismFile);
+    //phaseXmlNode->unlock();
+    //rootXmlNode->unlock();
+//    delete rootXmlNode; // this deletes all child nodes as well
+
+    delete kinetics;
+    delete multiTransport;
+    delete mixTransport;
+}
+
+
+void CanteraGas::initialize(bool multiTransportFlag)
+{
+    usingMultiTransport = multiTransportFlag;
+
+    // XML Information File
+    if (!boost::filesystem::exists(mechanismFile)) {
+        throw debugException("Error: Cantera input file \"" + mechanismFile + "\" not found.");
+    }
+
+    rootXmlNode = Cantera::get_XML_File(mechanismFile);
+    phaseXmlNode = rootXmlNode->findNameID("phase", phaseID);
+
+    // Initialize the default thermodynamic properties object
+    Cantera::importPhase(*phaseXmlNode, &thermo);
+
+    // Initialize the default chemical kinetics object
+    kinetics = new Cantera::GasKinetics(&thermo);
+    kinetics->init();
+    Cantera::installReactionArrays(*phaseXmlNode, *kinetics, phaseID);
+    kinetics->finalize();
+
+    // Initialize the default transport properties object
+    Cantera::TransportFactory* transFac = Cantera::TransportFactory::factory();
+    if (usingMultiTransport) {
+        multiTransport = dynamic_cast<Cantera::MultiTransport*>(transFac->newTransport("Multi",&thermo));
+        transFac->initTransport(multiTransport, &thermo);
+    } else {
+        mixTransport = dynamic_cast<Cantera::MixTransport*>(transFac->newTransport("Mix",&thermo));
+        transFac->initTransport(mixTransport, &thermo);
+    }
+    transFac->deleteFactory();
+
+    nSpec = thermo.nSpecies();
+}
+
+void CanteraGas::setStateMass(const dvector& Y_in, const double T)
+{
+    dvector Y(nSpec);
+    for (size_t k=0; k<nSpec; k++) {
+        Y[k] = max(Y[k], 0.0);
+    }
+    thermo.setState_TPY(T, pressure, &Y[0]);
+}
+
+void CanteraGas::setStateMole(const dvector& X_in, const double T)
+{
+    dvector X(nSpec);
+    for (size_t k=0; k<nSpec; k++) {
+        X[k] = max(X[k], 0.0);
+    }
+    thermo.setState_TPX(T, pressure, &X[0]);
+}
+
+void CanteraGas::getMoleFractions(dvector& X) const
+{
+    thermo.getMoleFractions(&X[0]);
+}
+
+void CanteraGas::getMassFractions(dvector& Y) const
+{
+    thermo.getMassFractions(&Y[0]);
+}
+
+double CanteraGas::getDensity() const
+{
+    return thermo.density();
+}
+
+double CanteraGas::getMixtureMolecularWeight() const
+{
+    return thermo.meanMolecularWeight();
+}
+
+void CanteraGas::getMolecularWeights(dvector& W) const
+{
+    for (size_t k=0; k<nSpec; k++) {
+        W[k] = thermo.molecularWeight(k);
+    }
+}
+
+double CanteraGas::getViscosity() const
+{
+    if (usingMultiTransport) {
+        return multiTransport->viscosity();
+    } else {
+        return mixTransport->viscosity();
+    }
+}
+
+double CanteraGas::getThermalConductivity() const
+{
+    if (usingMultiTransport) {
+        return multiTransport->thermalConductivity();
+    } else {
+        return mixTransport->thermalConductivity();
+    }
+}
+
+void CanteraGas::getDiffusionCoefficients(dvector& Dkm) const
+{
+    if (usingMultiTransport) {
+        multiTransport->getMixDiffCoeffs(&Dkm[0]);
+    } else {
+        mixTransport->getMixDiffCoeffs(&Dkm[0]);
+    }
+}
+
+void CanteraGas::getWeightedDiffusionCoefficients(dvector& rhoD) const
+{
+    if (usingMultiTransport) {
+        multiTransport->getMixDiffCoeffs(&rhoD[0]);
+        double rho = thermo.density();
+        for (size_t k=0; k<nSpec; k++) {
+            rhoD[k] *= rho;
+        }
+    } else {
+        mixTransport->getMixDiffCoeffs(&rhoD[0]);
+        double rho = thermo.density();
+        for (size_t k=0; k<nSpec; k++) {
+            rhoD[k] *= rho;
+        }
+    }
+}
+
+void CanteraGas::getThermalDiffusionCoefficients(dvector& Dkt) const
+{
+    if (usingMultiTransport) {
+        multiTransport->getThermalDiffCoeffs(&Dkt[0]);
+    } else {
+        mixTransport->getThermalDiffCoeffs(&Dkt[0]);
+    }
+}
+
+double CanteraGas::getSpecificHeatCapacity() const
+{
+    return thermo.cp_mass();
+}
+
+void CanteraGas::getSpecificHeatCapacities(dvector& cpSpec) const
+{
+    thermo.getPartialMolarCp(&cpSpec[0]);
+}
+
+void CanteraGas::getEnthalpies(dvector& hk) const
+{
+    thermo.getPartialMolarEnthalpies(&hk[0]);
+}
+
+void CanteraGas::getReactionRates(dvector& wDot) const
+{
+    kinetics->getNetProductionRates(&wDot[0]);
+}
---
+++ src/chemistry0d.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <cantera/Cantera.h>
+#include <cantera/IdealGasMix.h>    // defines class IdealGasMix
+#include <cantera/equilibrium.h>    // chemical equilibrium
+#include <cantera/thermo.h>
+#include <cantera/transport.h>      // transport properties
+#include <cantera/kinetics.h>
+
+#include <cantera/kernel/IdealGasPhase.h>
+#include <cantera/kernel/GasKinetics.h>
+#include <cantera/kernel/ctml.h>
+
+#include "mathUtils.h"
+
+#ifdef WIN32
+#define Cantera_CXX Cantera
+#endif
+
+class CanteraGas
+{
+    // This class groups together a set of Cantera objects needed for calculating
+    // thermodynamic properties, transport properties, and kinetic rates
+
+public:
+    CanteraGas();
+    ~CanteraGas();
+
+    std::string mechanismFile;
+    std::string phaseID;
+    double pressure; // thermodynamic pressure
+    size_t nSpec; // number of species
+
+    bool usingMultiTransport; // use multicomponent transport model? (vs. mixture-averaged)
+
+    void initialize(bool multiTransportFlag);
+
+    void setStateMass(const dvector& Y, const double T);
+    void setStateMole(const dvector& X, const double T);
+
+    void getMoleFractions(dvector& X) const;
+    void getMassFractions(dvector& Y) const;
+    double getDensity() const;
+    double getMixtureMolecularWeight() const;
+    void getMolecularWeights(dvector& W) const;
+
+    double getViscosity() const;
+    double getThermalConductivity() const;
+    void getDiffusionCoefficients(dvector& Dkm) const;
+    void getWeightedDiffusionCoefficients(dvector& rhoD) const;
+    void getThermalDiffusionCoefficients(dvector& Dkt) const;
+
+    double getSpecificHeatCapacity() const;
+    void getSpecificHeatCapacities(dvector& cpSpec) const;
+    void getEnthalpies(dvector& hk) const;
+
+    void getReactionRates(dvector& wDot) const;
+
+    Cantera::IdealGasPhase thermo;
+
+private:
+    Cantera::XML_Node* rootXmlNode;
+    Cantera::XML_Node* phaseXmlNode;
+
+    Cantera::GasKinetics* kinetics;
+    Cantera::MultiTransport* multiTransport;
+    Cantera::MixTransport* mixTransport;
+};
