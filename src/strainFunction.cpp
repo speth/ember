331b01ef7034237f306b2d02d84f03a53c22ae21
Ray Speth <speth@mit.edu>
2010-06-23 16:35:14 -0400

    Fixed const disagreement in declaration and definition of strainFunction members.

--- src/strainFunction.cpp
+++ src/strainFunction.cpp
@@ -1,11 +1,13 @@
 #include "strainFunction.h"
 
-double StrainFunction::a(double t) {
+double StrainFunction::a(double t) const
+{
     // TODO: implement configurable strain rate
     return 100;
 }
 
-double StrainFunction::dadt(double t) {
+double StrainFunction::dadt(double t) const
+{
     // TODO: implement configurable strain rate
     return 0;
 }
