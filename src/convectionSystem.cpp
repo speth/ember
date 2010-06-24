2fcb4ea23a690acc06ac52e56e133ccb2e897a19
Ray Speth <speth@mit.edu>
2010-06-24 18:28:16 -0400

    Added framework for dealing with ConvectionSystem

--- src/convectionSystem.cpp
+++ src/convectionSystem.cpp
@@ -1,1 +1,49 @@
 #include "convectionSystem.h"
+
+int ConvectionSystem::f(const realtype t, const sdVector& y, sdVector& ydot)
+{
+    unroll_y(y);
+    // *** Update auxiliary data ***
+
+    // *** Calculate V ***
+
+    // *** Calculate dY/dt, dU/dt, dT/dt
+
+    roll_ydot(ydot);
+    return 0;
+}
+
+
+void ConvectionSystem::unroll_y(const sdVector& y)
+{
+    for (size_t j=0; j<nPoints; j++) {
+        T[j] = y[j*nVars+kEnergy];
+        U[j] = y[j*nVars+kMomentum];
+        for (size_t k=0; k<nSpec; k++) {
+            Y[j][k] = y[j*nVars+kSpecies+k];
+        }
+    }
+}
+
+void ConvectionSystem::roll_y(sdVector& y) const
+{
+    for (size_t j=0; j<nPoints; j++) {
+        y[j*nVars+kEnergy] = T[j];
+        y[j*nVars+kMomentum] = U[j];
+        for (size_t k=0; k<nSpec; k++) {
+            y[j*nVars+kSpecies+k] = Y[j][k];
+        }
+    }
+}
+
+void ConvectionSystem::roll_ydot(sdVector& ydot) const
+{
+    for (size_t j=0; j<nPoints; j++) {
+        ydot[j*nVars+kEnergy] = dTdt[j];
+        ydot[j*nVars+kMomentum] = dUdt[j];
+        for (size_t k=0; k<nSpec; k++) {
+            ydot[j*nVars+kSpecies+k] = dYdt[j][k];
+        }
+    }
+
+}
--- src/convectionSystem.h
+++ src/convectionSystem.h
@@ -4,16 +4,33 @@
 #include "sundialsUtils.h"
 #include "strainFunction.h"
 #include "grid.h"
+#include "chemistry0d.h"
+
 
 class ConvectionSystem  : public sdODE, public GridBased
 {
     // This is the system representing convection of all state variables in the domain.
 public:
     // The ODE function: ydot = f(t,y)
-    int f(realtype t, sdVector& y, sdVector& ydot);
+    int f(const realtype t, const sdVector& y, sdVector& ydot);
     // This uses an explicit integrator, so no Jacobian/Preconditioner is necessary
 
+    void unroll_y(const sdVector& y); // fill in current state variables from sdvector
+    void roll_y(sdVector& y) const; // fill in sdvector with current state variables
+    void roll_ydot(sdVector& ydot) const; // fill in sdvector with current time derivatives
+
+    dvector U, dUdt;
+    dvector T, dTdt;
+    vector<dvector> Y, dYdt;
+
+    size_t nSpec;
+    size_t nPoints;
+    size_t nVars; // == nSpec + 2
+
 private:
+    // Cantera data
+    CanteraGas* gas;
+
     dvector drhodt;
     dvector Wmx; // mixture molecular weight [kg/kmol]
     dvector rV; // (radial) mass flux (r*V) [kg/m^2*s or kg/m*rad*s]
