c11131a3815aee57a48375c27d847825295933a8
Ray Speth <speth@mit.edu>
2010-06-24 17:37:49 -0400

    Initial implementation of DiffusionSystem.

--- src/diffusionSystem.cpp
+++ src/diffusionSystem.cpp
@@ -1,1 +1,46 @@
 #include "diffusionSystem.h"
+
+void DiffusionSystem::get_J(sdBandMatrix& J)
+{
+    SetToZero(J.forSundials());
+    int N = D.size();
+    dvector c1(N);
+    dvector c2(N);
+    dvector c3(N);
+    for (int j=0; j<N-1; j++) {
+        c1[j] = 0.5/(dlj[j]*r[j]*B[j]);
+        c2[j] = rphalf[j]*(D[j]+D[j+1])/hh[j];
+    }
+
+    // left boundary value
+    int jStart;
+    if (leftBC == BoundaryCondition::FixedValue) {
+        jStart = 1;
+    } else  { // (leftBC == BoundaryCondition::ZeroGradient)
+        jStart = 2;
+        J(1,1) = -c1[1]*c2[1];
+        J(1,2) = c1[1]*c2[1];
+    }
+
+    // right boundary value
+    int jStop;
+    if (rightBC == BoundaryCondition::FixedValue) {
+        jStop = N-1;
+    } else { // (rightBC == BoundaryCondition::ZeroGradient)
+        jStop = N-2;
+        J(N-2,N-3) = c1[N-2]*c2[N-3];
+        J(N-2,N-2) = -c1[N-2]*c2[N-3];
+    }
+
+    // intermediate points
+    for (int j=jStart; j<jStop; j++) {
+        J(j,j-1) = c1[j]*c2[j-1];
+        J(j,j) = -c1[j]*(c2[j-1] + c2[j]);
+        J(j,j+1) = c1[j]*c2[j];
+    }
+}
+
+void DiffusionSystem::get_c(dvector& other_c)
+{
+    other_c.assign(C.begin(), C.end());
+}
--- src/diffusionSystem.h
+++ src/diffusionSystem.h
@@ -4,25 +4,37 @@
 #include "sundialsUtils.h"
 #include "chemistry0d.h"
 #include "grid.h"
+#include "integrator.h"
 
-class DiffusionSystem : public sdODE, public GridBased
+namespace BoundaryCondition {
+    enum BC { FixedValue, ZeroGradient };
+}
+
+class DiffusionSystem : public LinearODE, public GridBased
 {
-    // This is a system representing diffusion of a single solution component
-public:
-    // Evaluate an ODE of the form:
-    // ydot = B * d/dx(D * dy/dx) + constantTerm
-    int f(realtype t, sdVector& y, sdVector& ydot);
+    // This is a system representing diffusion of a single solution component,
+    // represented by an ODE in one of the following forms:
+    //     ydot = B * d/dx(D * dy/dx) + C
+    //     ydot = B/r * d/dr(r * D * dy/dr) + C
 
-    int bandedJacobian(realtype t, sdVector& y, sdVector& ydot, sdBandMatrix& J);
+public:
+
+    // The Jacobian matrix assoicated with the ODE, requested by the integrator
+    void get_J(sdBandMatrix& J);
+
+    // provides the constant term to the integrator
+    void get_c(dvector& y);
 
     // The current solution vector
     dvector y;
 
     // the coefficients of the ODE
-    dvector B;
-    dvector D;
+    dvector B; // pre-factor
+    dvector C; // constant term
+    dvector D; // "diffusion" coefficient
+
+    BoundaryCondition::BC leftBC;
+    BoundaryCondition::BC rightBC;
 
 private:
-    // The sum of the terms held constant for this system
-    dvector constantTerm;
 };
