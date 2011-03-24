cc668f7aaf2aa9f0a3ecbe6940d72d02e9079e46
Ray Speth <speth@mit.edu>
2011-03-24 14:37:19 -0400

    Added example input file for multiprocessing

---
+++ input/example-parallel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#$ -pe singlenode 8
+#$ -l normal
+#$ -l h_rt=120:00:00
+#$ -j y
+#$ -o job-output-example-parallel.txt
+
+import multiprocessing
+from pyro import *
+
+outputDir = 'run/example-parallel-phi%4.2f'
+logFile = 'out-example-parallel-phi%4.2f'
+
+def start(phi):
+    conf = Config(
+        Paths(outputDir=outputDir % phi,
+              logFile=logFile % phi),
+        InitialCondition(equivalenceRatio=phi),
+        StrainParameters(initial=50,
+                         final=50),
+        Times(profileStepInterval=1000000,
+              profileTimeInterval=5e-3))
+
+    run(conf)
+
+pool = multiprocessing.Pool(8)
+pool.map(start, [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.90, 0.95])
+print 'All done!'
