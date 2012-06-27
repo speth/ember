6acfed3ab801627004cfff3d438328f19ba444ca
Ray Speth <speth@mit.edu>
2012-06-27 17:33:39 -0400

    Better layout of configuration option UI

--- python/pyro/gui.py
+++ python/pyro/gui.py
@@ -5,17 +5,15 @@ from PySide import QtGui
 import utils
 import input
 
-
 class OptionWidget(QtGui.QWidget):
-    def __init__(self, name, opt, *args, **kwargs):
+    def __init__(self, label, opt, *args, **kwargs):
         QtGui.QWidget.__init__(self)
 
-        self.optName = name
         self.opt = opt
-
+        self.label = label
+        self.optName = self.label.text()
         self.setLayout(QtGui.QHBoxLayout())
-        self.label = QtGui.QLabel(name)
-        self.layout().addWidget(self.label)
+        self.layout().setContentsMargins(0,0,0,0)
 
     def checkDefault(self):
         if self.opt.value != self.opt.default:
@@ -25,8 +23,8 @@ class OptionWidget(QtGui.QWidget):
 
 
 class StringOptionWidget(OptionWidget):
-    def __init__(self, name, opt, *args, **kwargs):
-        OptionWidget.__init__(self, name, opt)
+    def __init__(self, label, opt, *args, **kwargs):
+        OptionWidget.__init__(self, label, opt)
         self.text = QtGui.QLineEdit(opt.value)
         self.layout().addWidget(self.text)
         self.text.textChanged.connect(self.updateOpt)
@@ -36,9 +34,24 @@ class StringOptionWidget(OptionWidget):
         self.checkDefault()
 
 
+class NumericOptionWidget(OptionWidget):
+    def __init__(self, label, opt, *args, **kwargs):
+        OptionWidget.__init__(self, label, opt)
+        self.text = QtGui.QLineEdit(str(opt.value))
+        self.layout().addWidget(self.text)
+        self.text.textChanged.connect(self.updateOpt)
+
+    def updateOpt(self):
+        try:
+            self.opt.value = float(self.text.text())
+        except ValueError:
+            pass
+        self.checkDefault()
+
+
 class BoolOptionWidget(OptionWidget):
-    def __init__(self, name, opt, *args, **kwargs):
-        OptionWidget.__init__(self, name, opt)
+    def __init__(self, label, opt, *args, **kwargs):
+        OptionWidget.__init__(self, label, opt)
 
         self.trueWidget = QtGui.QRadioButton('True')
         self.falseWidget = QtGui.QRadioButton('False')
@@ -50,6 +63,7 @@ class BoolOptionWidget(OptionWidget):
         self.layout().addWidget(self.falseWidget)
 
         self.trueWidget.toggled.connect(self.updateOpt)
+        self.layout().addStretch(1)
 
     def updateOpt(self):
         self.opt.value = self.trueWidget.isChecked()
@@ -57,8 +71,8 @@ class BoolOptionWidget(OptionWidget):
 
 
 class EnumOptionWidget(OptionWidget):
-    def __init__(self, name, opt, *args, **kwargs):
-        OptionWidget.__init__(self, name, opt)
+    def __init__(self, label, opt, *args, **kwargs):
+        OptionWidget.__init__(self, label, opt)
 
         self.combo = QtGui.QComboBox()
         self.items = {}
@@ -82,18 +96,72 @@ class OptionsWidget(QtGui.QGroupBox):
     def __init__(self, opts, *args, **kwargs):
         QtGui.QGroupBox.__init__(self)
         self.opts = opts
-        self.setLayout(QtGui.QVBoxLayout())
+        self.setLayout(QtGui.QGridLayout())
+        self.layout().setSpacing(0)
         self.setTitle(self.opts.__class__.__name__)
-        for name,opt in self.opts:
+
+        width = 0
+        for i,(name,opt) in enumerate(self.opts):
+            label = QtGui.QLabel(name)
+            self.layout().addWidget(label, i, 0)
+            width = max(label.sizeHint().width(), width)
+
             if opt.choices is not None:
-                w = EnumOptionWidget(name, opt)
+                w = EnumOptionWidget(label, opt)
             elif isinstance(opt, input.StringOption):
-                w = StringOptionWidget(name, opt)
+                w = StringOptionWidget(label, opt)
+            elif isinstance(opt, (input.FloatOption, input.IntegerOption)):
+                w = NumericOptionWidget(label, opt)
             elif isinstance(opt, input.BoolOption):
-                w = BoolOptionWidget(name, opt)
+                w = BoolOptionWidget(label, opt)
             else:
-                w = QtGui.QLabel(name + ' = ' + str(opt.value))
+                w = QtGui.QLabel(str(opt.value))
+
+            self.layout().addWidget(w, i, 1)
+
+        self.layout().setVerticalSpacing(4)
+        self.layout().setColumnMinimumWidth(0, width + 5)
+        spacer = QtGui.QSpacerItem(1, 1000, QtGui.QSizePolicy.Minimum,
+                                   QtGui.QSizePolicy.Maximum)
+        self.layout().addItem(spacer, i+1, 0)
+
+
+class MultiOptionsWidget(QtGui.QWidget):
+    def __init__(self, conf, *args, **kwargs):
+        QtGui.QWidget.__init__(self)
+        self.conf = conf
+        self.setLayout(QtGui.QHBoxLayout())
+        self.optionsList = QtGui.QListWidget()
+        self.optionsList.setSpacing(1)
+        self.layout().addWidget(self.optionsList)
+        self.activeOptionWidget = None
+
+        height = 0
+        for item in self.conf:
+            listitem = QtGui.QListWidgetItem(item.__class__.__name__)
+            self.optionsList.addItem(listitem)
+            w = OptionsWidget(item)
+            height = max(height, w.minimumSizeHint().height())
+            listitem.widget = w
             self.layout().addWidget(w)
+            w.hide()
+
+        self.optionsList.setCurrentRow(0)
+        self.setActiveWidget(self.optionsList.currentItem())
+        self.setMinimumHeight(height)
+        width = self.optionsList.sizeHintForColumn(0) + 5
+        self.optionsList.setMinimumWidth(width)
+        self.optionsList.setMaximumWidth(width)
+        self.optionsList.currentItemChanged.connect(self.setActiveWidget)
+        self.optionsList.setSizePolicy(QtGui.QSizePolicy.Fixed,
+                                       QtGui.QSizePolicy.Preferred)
+
+    def setActiveWidget(self, listitem):
+        if self.activeOptionWidget is not None:
+            self.activeOptionWidget.hide()
+
+        self.activeOptionWidget = listitem.widget
+        self.activeOptionWidget.show()
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -118,15 +186,8 @@ class MainWindow(QtGui.QMainWindow):
 
     def newConf(self):
         self.conf = input.Config()
-        w = QtGui.QWidget()
-        w.setLayout(QtGui.QVBoxLayout())
-        for item in self.conf:
-            w2 = OptionsWidget(item)
-            w.layout().addWidget(w2)
-
-        scroll = QtGui.QScrollArea()
-        scroll.setWidget(w)
-        self.setCentralWidget(scroll)
+        w = MultiOptionsWidget(self.conf)
+        self.setCentralWidget(w)
 
 
 def main():
