07913c9f9baeec83a23166355fccd4f7c18dcabd
Ray Speth <speth@mit.edu>
2013-03-14 21:24:11 -0400

    Removed code for the Boost.Python module

--- src/python/module.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <boost/python.hpp>
-
-// The function exportFoo is defined in pyFOO.cpp
-void exportReadConfig();
-void exportConverters();
-void exportContainers();
-void exportFlameSolver();
