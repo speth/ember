57513137902cbde3c1a59c2ff4ef38a774491d75
Ray Speth <speth@mit.edu>
2013-03-26 19:16:38 -0400

    Added Doxygen docs for ConvectionSystemSplit

--- src/convectionSystem.h
+++ src/convectionSystem.h
@@ -142,52 +142,85 @@ private:
 };
 
 
+//! Composite system representing the Convection term for all components.
+/*!
+ * System which combines a ConvectionSystemUTW object and several
+ * ConvectionSystemY objects that together represent the complete convection
+ * term for all components.
+ */
 class ConvectionSystemSplit : public GridBased
 {
-    // System which combines a ConvectionSystemUTW objects and several
-    // ConvectionSystemY objects that together represent the complete
-    // convection term for all components.
 public:
     ConvectionSystemSplit();
 
     void setGrid(const OneDimGrid& grid);
-    void setTolerances(const ConfigOptions& options);
-    void setGas(CanteraGas& gas);
-    void resize(const size_t nPoints, const size_t nSpec, dmatrix& state);
-    void setState(double tInitial);
-    void setLeftBC(const double Tleft, const dvec& Yleft);
-    void set_rVzero(const double rVzero);
-    void evaluate(); // evaluate time derivatives and mass flux at the current state
 
-    // Time derivatives of species and temperature from the other split terms are needed
-    // to correctly compute the density derivative appearing in the continuity equation
+    //! Set tolerances for the CVODE solvers.
+    void setTolerances(const ConfigOptions& options);
+
+    //! Set the Cantera object used for property evaluations.
+    void setGas(CanteraGas& gas);
+
+    //! Set the problem size and provide values for the current state variables.
+    void resize(const size_t nPoints, const size_t nSpec, dmatrix& state);
+
+    //! Set the state of the internal solvers from the state of the composite
+    //! solver.
+    void setState(double tInitial);
+
+    //! Set boundary conditions for temperature and mass fractions left of the
+    //! domain when using BoundaryCondition::ControlVolume or
+    //! BoundaryCondition::WallFlux.
+    void setLeftBC(const double Tleft, const dvec& Yleft);
+
+    //! Set the mass flux boundary value at j=0.
+    void set_rVzero(const double rVzero);
+
+    //! Evaluate time derivatives for each state variable and the normal mass
+    //! flux at the current state.
+    void evaluate();
+
+    //! Set the time derivative of the density. Time derivatives of species
+    //! and temperature from the other split terms are needed to correctly
+    //! compute the density derivative appearing in the continuity equation.
     void setDensityDerivative(const dvec& drhodt);
 
-    // Constants introduced by the splitting method
+    //! Set the constants introduced by the splitting method.
     void setSplitConstants(const dmatrix& splitConst);
 
+    //! Set the constants introduced by the splitting method to zero.
     void resetSplitConstants();
 
+    //! Integrate the the split convection equations to *tf*.
     void integrateToTime(const double tf);
-    void unroll_y(); // convert the solver's solution vectors to the full U, Y, and T
+
+    //! convert the solver's solution vectors to the full *U*, *Y*, and *T*.
+    void unroll_y();
+
+    //! Compute the total number of timesteps taken by the solvers for the
+    //! individual components.
     int getNumSteps();
+
+    //! Set the velocity field data used in the Quasi2D case.
     void setupQuasi2D(boost::shared_ptr<BilinearInterpolator>& vzInterp,
                       boost::shared_ptr<BilinearInterpolator>& vrInterp);
 
-    VecMap U;
-    VecMap T;
-    MatrixMap Y;
-    dvec Wmx;
+    VecMap U; //!< normalized tangential velocity (u*a/u_inf) [1/s]
+    VecMap T; //!< temperature [K]
+    MatrixMap Y; //!< species mass fractions, Y(k,j) [-]
+    dvec Wmx; //!< Mixture molecular weight [kg/kmol]
 
-    // Time derivatives and mass flux are updated by evaluate()
-    dvec V;
-    dvec dUdt;
-    dvec dTdt;
-    dvec dWdt;
-    dmatrix dYdt;
+    // Time derivatives and mass flux are updated by evaluate()BoundaryCondition::ControlVolume and BoundaryCondition::WallFlux
+    dvec V; //!< mass flux normal to the flame [kg/m^2*s]
+    dvec dUdt; //!< Time derivative of #U [1/s^2]
+    dvec dTdt; //!< Time derivative of #T [K/s]
+    dvec dWdt; //!< Time derivative of #Wmx [kg/kmol*s]
+    dmatrix dYdt; //!< Time derivative of #Y [1/s]
 
+    //! System used to solve for #U, #T, and #Wmx
     ConvectionSystemUTW utwSystem;
 
+    //! Interpolation data for #V, used for the species solvers
     boost::shared_ptr<vecInterpolator> vInterp;
 
     PerfTimer utwTimer, speciesTimer;
@@ -196,26 +229,36 @@ private:
     // set parameters of a new species solver
     void configureSolver(SundialsCvode& solver, const size_t k);
 
-    // CVODE integration tolerances
-    double reltol; // relative tolerance
-    double abstolU; // velocity absolute tolerance
-    double abstolT; // temperature absolute tolerance
-    double abstolW; // molecular weight absolute tolerance
-    double abstolY; // mass fraction absolute tolerance
+    //! CVODE integration tolerances
+    double reltol; //!< relative integrator tolerance
+    double abstolU; //!< velocity absolute tolerance
+    double abstolT; //!< temperature absolute tolerance
+    double abstolW; //!< molecular weight absolute tolerance
+    double abstolY; //!< mass fraction absolute tolerance
 
+    //! Solver for #utwSystem
     boost::shared_ptr<SundialsCvode> utwSolver;
+
+    //! Systems used to solve the convection term for each species.
     boost::ptr_vector<ConvectionSystemY> speciesSystems;
+
+    //! Solvers for system in #speciesSystems
     boost::ptr_vector<SundialsCvode> speciesSolvers;
 
+    //! Mass fraction left of the domain. Used with
+    //! BoundaryCondition::ControlVolume and BoundaryCondition::WallFlux.
     dvec Yleft;
-    dvec W;
 
-    size_t nSpec;
-    size_t nVars;
+    dvec W; //!< Molecular weight of each species [kg/kmol]
 
-    CanteraGas* gas;
+    size_t nSpec; //!< Number of species
+    size_t nVars; //!< Number of state variables in the UTW system (`==3`)
 
+    CanteraGas* gas; //!< Cantera object used for computing #Wmx
+
+    //! `true` when solving the quasi-2d problem; `false` otherwise.
     bool quasi2d;
+
     friend class ConvectionTermWrapper;
 };
 
