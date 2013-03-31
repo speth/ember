6ef4fd1880f1f4bc13dc65d84a9913774de2090b
Ray Speth <speth@mit.edu>
2013-03-30 21:49:24 -0400

    Added Doxygen docs for class DataFile

--- src/dataFile.h
+++ src/dataFile.h
@@ -9,29 +9,55 @@
 
 #include "mathUtils.h"
 
+//! Represents an HDF5 data file
 class DataFile
 {
 public:
-    DataFile(void);
+    DataFile();
+
+    //! Create and open an HDF5 file.
+    //! @copydetails open()
     explicit DataFile(const std::string& filename,
                       std::ios_base::openmode mode=std::ios_base::app);
-    ~DataFile(void);
 
+    ~DataFile();
+
+    //! Add a single scalar named *name* with value *x*.
     void writeScalar(const std::string& name, const double x);
+
+    //! Read a scalar named *name*
     double readScalar(const std::string& name);
 
+    //! Write a 1D array (`std::vector<double>`) named *name* with values *v*.
     void writeVector(const std::string& name, const dvector& v);
+
+    //! Read the contents of a 1D array named *name* as a `std::vector<double>`.
     dvector readVector(const std::string& name);
 
+    //! Write a 1D array (`Eigen::ArrayXd`) named *name* with values *v*.
     void writeVec(const std::string& name, const dvec& v);
+
+    //! Read the contents of a 1D array named *name* as an `Eigen::ArrayXd`.
     dvec readVec(const std::string& name);
 
-
+    //! Write a 2D array (`Eigen::ArrayXXd`) named *name* with values *y*.
+    //! Set `transpose` to `true` to change the orientation of the matrix.
     void writeArray2D(const std::string& name, const dmatrix& y, bool transpose=false);
+
+    //! Read a 2D array (`Eigen::ArrayXXd`) named *name*.
+    //! Set `transpose` to `true` to change the orientation of the matrix.
     dmatrix readArray2D(const std::string& name, bool transpose=false);
 
+    //! Open an HDF5 data file.
+    //! @param filename The name of the file. The file will be created if it
+    //!     does not already exist.
+    //! @param mode May be either `std::ios_base::app` to append to an
+    //!     existing file or `std::ios_base::trunc` to truncate and discard
+    //!     any existing contents.
     void open(const std::string& filename,
               std::ios_base::openmode mode=std::ios_base::app);
+
+    //! Close the associated file.
     void close();
 
 private:
