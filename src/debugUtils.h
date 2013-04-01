cbae65bb80f1b785d60e05492d144aededf75992
Ray Speth <speth@mit.edu>
2013-03-31 23:12:10 -0400

    Added Doxygen docs for classes declared in debugUtils.h

--- src/debugUtils.h
+++ src/debugUtils.h
@@ -7,28 +7,58 @@
 #include <boost/format.hpp>
 using boost::format;
 
+//! A container for global flags that set the verbosity of log messages.
 class debugParameters
 {
 public:
+    //! Set values for the various debug flags. This function exists because
+    //! it's easier to expose in Cython than the individual static members of
+    //! this class.
     static void setParameters(bool adapt, bool regrid, bool timesteps,
                               bool radius, bool verbose);
+
+    //! Print verbose information while doing grid adaptation (adding and
+    //! removing interior grid points).
     static bool debugAdapt;
+
+    //! Print verbose information while regridding (adding and removing
+    //! boundary grid points).
     static bool debugRegrid;
+
+    //! Print a single summary line after each global timestep.
     static bool debugTimesteps;
+
+    //! Print information related to the feedback control used to set the
+    //! location of the flame, when this capability is active.
     static bool debugFlameRadiusControl;
+
+    //! Print lots of extra information, primarily about the progress of the
+    //! individual solvers for the split terms.
     static bool veryVerbose;
 };
 
+//! Write logging information either to a file or to `stdout` if no output
+//! file has been specified. Usually accessed through the global instance
+//! `logFile`.
 class LogFile
 {
 public:
+    //! Create a logger that writes to `stdout`.
     LogFile();
+    //! Create a logger that writes to the file named *filename*. An existing
+    //! file with the same name will be replaced.
     explicit LogFile(const std::string& filename);
     ~LogFile();
 
+    //! Direct any further output to the file named *filename*.
     void open(const std::string& filename);
+
+    //! Close the current log file, directing any further output to `stdout`.
     void close();
 
+    //! Write the value of *other* to the current log destination. Templated
+    //! so that any object that overloads `operator<<` for `std::ostream` will
+    //! be appropriately formatted.
     template <class T>
     void write(const T& other, bool newline=true) {
         if (haveFile) {
@@ -48,6 +78,8 @@ public:
         }
     }
 
+    //! Write the value of *message* to the current log destination only if
+    //! the flag `debugParameters::veryVerbose` is set.
     template <class T>
     void verboseWrite(const T& message, bool newline=true) {
         if (debugParameters::veryVerbose) {
@@ -60,8 +92,10 @@ private:
     std::ofstream file;
 };
 
+//! Global LogFile instance
 extern LogFile logFile;
 
+//! Class for exceptions raised by Ember
 class DebugException : public std::exception
 {
 public:
