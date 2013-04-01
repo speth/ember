00ca57febd4f2c6654e6ff3d0135c355727f831b
Ray Speth <speth@mit.edu>
2013-04-01 00:11:38 -0400

    Added Doxygen docs for classes declared in integrator.h

--- src/integrator.h
+++ src/integrator.h
@@ -2,11 +2,13 @@
 
 #include "mathUtils.h"
 
+//! Abstract base class for an %ODE initial value problem that can be
+//! integrated by an Integrator.
 class ODE
 {
 public:
     virtual ~ODE() {}
-    // ODE function defined as ydot = f(t,y)
+    //! %ODE function defined as `ydot = f(t,y)`
     virtual void f(const double t, const dvec& y, dvec& ydot) = 0;
 };
 
@@ -34,40 +36,57 @@ public:
     virtual void initialize() {}
 };
 
-
+//! Abstract base class for an %ODE integrator that can integrate instances of
+//! class ODE.
 class Integrator
 {
 public:
     Integrator();
     virtual ~Integrator() {}
 
-    // Initialization - Each of these must be called before starting integration
+    //! Set the initial condition for the problem. Must be called before
+    //! starting integration.
     virtual void set_y0(const dvec& y0);
+
+    //! Set up parameters and problem-dependent data structures for the
+    //! solver. Must be called before starting integration.
+    //! @param t0 Start time for the integration
+    //! @param h initial step size
     virtual void initialize(const double t0, const double h);
 
-    // Accessor functions
+    //! Get the last step size used
     double get_h() const;
+
+    //! Get the current time reached by the integrator
     double get_t() const;
+
+    //! Get the current state vector
     virtual const dvec& get_y() const;
+
+    //! Get the time derivative of the current state vector
     virtual const dvec& get_ydot() = 0;
 
-    // Actually do the integration
-    virtual void step() = 0; // take a single step
+    //! Take a single step using the current step size.
+    virtual void step() = 0;
+
+    //! Take as many steps as necessary to reach *tEnd* without stepping past
+    //! it.
     virtual void integrateToTime(double tEnd);
 
-    dvec y; // solution vector
-    dvec ydot; // derivative of state vector
-    double t; // current time
+    dvec y; //!< solution vector
+    dvec ydot; //!< derivative of state vector
+    double t; //!< current time
 
 protected:
-    double h; // timestep
-    size_t N; // Dimension of y
+    double h; //!< timestep
+    size_t N; //!< Dimension of y
 };
 
-
+//! Integrates an %ODE defined as `ydot = f(t,y)` using the explicit Euler
+//! method. This class exists mostly as a demonstration of how to implement
+//! Integrator.
 class ExplicitIntegrator : public Integrator
 {
-    // Integrates an ODE defined as ydot = f(t,y) using Euler's method
 public:
     ExplicitIntegrator(ODE& ode);
     ~ExplicitIntegrator() {}
@@ -75,40 +94,40 @@ public:
     void set_y0(const dvec& y0);
     const dvec& get_ydot();
 
-    // Actually do the integration
-    void step(); // take a single step
+    void step();
 
 private:
     ODE& myODE;
-    dvec ydot; // time derivative of solution vector
+    dvec ydot;
 };
 
-// Integrator using 1st and 2nd order BDF for tridiagonal systems,
-// with matrix inversions done using the Thomas algorithm.
+//! Integrator using 1st and 2nd order BDF for tridiagonal systems, with
+//! matrix inversions done using the Thomas algorithm.
 class TridiagonalIntegrator : public Integrator
 {
 public:
     TridiagonalIntegrator(TridiagonalODE& ode);
     virtual ~TridiagonalIntegrator() {}
 
-    // Setup
     void set_y0(const dvec& y0);
     void resize(size_t N_in);
     void initialize(const double t0, const double h);
 
     const dvec& get_ydot();
 
-    // Actually do the integration
-    void step(); // take a single step
+    void step();
 
 private:
     TridiagonalODE& myODE;
-    dvec a, b, c; // subdiagonal, diagonal, superdiagonal, right-hand-side
-    dvec k; // constant contribution to y'
+
+    dvec a; //!< subdiagonal components of right-hand-side
+    dvec b; //!< diagonal components of right-hand-side
+    dvec c; //!< superdiagonal components of right-hand-side
+    dvec k; //!< constant contribution to `y'`
     dvec lu_b, lu_c, lu_d; // matrix elements after factorization
     dvec invDenom_; // internal
-    unsigned int stepCount; // the number of steps taken since last initialization
+    unsigned int stepCount; //!< the number of steps taken since last initialization
 
-    int N; // The system size
-    dvec yprev; // previous solution
+    int N; //!< The system size
+    dvec yprev; //!< previous solution
 };
