4c3bbd71fcfeb6769e72dd7910520c6d3773d8e8
Ray Speth <speth@mit.edu>
2014-05-30 11:24:06 -0400

    Add tests for mathUtils::integrate

--- test/test_mathUtils.cpp
+++ test/test_mathUtils.cpp
@@ -41,3 +41,20 @@ TEST_F(TestVectorOps, minval)
     EXPECT_EQ(-100, mathUtils::minval(data, 0, 10));
     EXPECT_EQ(5, mathUtils::minval(data, 5, 19));
 }
+
+TEST(TestSplines, integrate_eigen)
+{
+    dvec x = dvec::LinSpaced(31, 0.0, 3.0);
+    dvec y = x.pow(2);
+    EXPECT_NEAR(mathUtils::integrate(x, y), 9.0, 1e-4);
+}
+
+TEST(TestSplines, integrate_stdvector)
+{
+    std::vector<double> x(31), y(31);
+    for (size_t i=0; i < 31; i++) {
+        x[i] = 0.1 * i;
+        y[i] = pow(x[i], 2);
+    }
+    EXPECT_NEAR(mathUtils::integrate(x, y), 9.0, 1e-4);
+}
